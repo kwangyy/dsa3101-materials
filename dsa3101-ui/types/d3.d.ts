7a6a3d58b8016be233febb18cadaf8f8537f0fe4
kwangyy <70025553+kwangyy@users.noreply.github.com>
2024-11-13 17:04:59 +0800

    Requirements + eslint rules

---
+++ dsa3101-ui/types/d3.d.ts
@@ -0,0 +1,7 @@
+declare module 'd3' {
+    export * from 'd3-selection';
+    export * from 'd3-drag';
+    export * from 'd3-force';
+    export * from 'd3-zoom';
+    export * from 'd3-scale';
+} 
